194a6ba2f0db9c4a8399ed568db96c2ece3b6402
khanhcao99 <khanhboy1324@gmail.com>
2022-06-23 15:40:04 +0700

    Read_File_Example

--- src/Read_File_Example/ReadFileExample.java
+++ src/Read_File_Example/ReadFileExample.java
@@ -13,8 +13,11 @@ public class ReadFileExample {
             bufferedWriter.write("1\n");
             bufferedWriter.write("2\n");
             bufferedWriter.write("3\n");
-            bufferedWriter.write("4\n");
+            bufferedWriter.write("22\n");
             bufferedWriter.write("5\n");
+            bufferedWriter.write("4\n");
+            bufferedWriter.write("33\n");
+            bufferedWriter.write("7\n");
         } catch (IOException e) {
             System.err.println("Không tạo được file");
         }
@@ -26,12 +29,16 @@ public class ReadFileExample {
 
             String index = "";
             int sum = 0;
-
+            int max = 0;
             while ((index = bufferedReader.readLine()) != null ){
                 System.out.println(index);
+                if (Integer.parseInt(index) > max){
+                    max = Integer.parseInt(index);
+                }
                 sum += Integer.parseInt(index);
             }
             System.out.println("Tổng các số trong file là= " + sum);
+            System.out.println("Số lớn nhất trong file là= " + max);
 
         }catch (IOException e1){
             System.out.println("File bị lỗi");
